bdce4cd2bc7af092c51abc4b617fe5a88bde84ef
Andras Nemeseri <andras@nemeseri.comm>
2024-10-26 09:13:19 -0700

    Loading animation and some extra logic

--- src/components/GalleryDialog.tsx
+++ src/components/GalleryDialog.tsx
@@ -7,8 +7,9 @@ type GalleryDialogProps = {
 }
 
 export default function GalleryDialog({ photo, handleClose, handleKeys }: GalleryDialogProps) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const timeoutRef = useRef<number | undefined>();
   const isOpen = !!photo;
 
   useEffect(() =>{
@@ -20,13 +21,32 @@ export default function GalleryDialog({ photo, handleClose, handleKeys }: Galler
   }, [isOpen]);
 
   useEffect(() => {
+    // delay the loader animation to prevent flicker on fast img loads
+    if (isLoading) {
+      timeoutRef.current = window.setTimeout(() => {
+        if (dialogRef.current) {
+          dialogRef.current.classList.add('loading');
+        }
+      }, 200);
+    } else {
+      if (dialogRef.current) {
+        dialogRef.current.classList.remove('loading');
+      }
+    }
+
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, [isLoading]);
+
+  useEffect(() => {
     setIsLoading(true);
   }, [photo]);
 
   return (
-    <dialog onClick={handleClose} onKeyDown={handleKeys} ref={dialogRef} className={isLoading ? 'loading' : ''}>
+    <dialog onClick={handleClose} onKeyDown={handleKeys} ref={dialogRef}>
       <button>Close</button>
-      <div className='img-skeleton'></div>
+      <div className='loader'></div>
       {photo && 
         <img src={photo.largeUrl} 
           key={photo.id} 
